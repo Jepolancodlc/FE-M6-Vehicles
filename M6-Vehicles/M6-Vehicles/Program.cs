949345304862737aed2e929ea07aec6a7a289719
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2021-01-15 15:22:21 +0100

    Revert "Revert "Revert "Delete M6-Vehicles directory"""
    This reverts commit 6d97f6eb22fa86a75bd6d6ea84ff4d428cc9b8a6.

---
+++ M6-Vehicles/M6-Vehicles/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace M6_Vehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Milestone1 m1 = new Milestone1();
+            m1.exe1();
+        }
+    }
+}
